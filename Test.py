acb492a6191cbc423d45114568c4f178a07ca565
LaikaLEET <71174057+LaikaLEET@users.noreply.github.com>
2021-05-28 04:05:02 +0300

    Update
    Added mysql insert.

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import pymysql.cursors
 def data(url):
     url_general = 'https://www.rusprofile.ru/'
     response = requests.get(url)
@@ -21,7 +22,9 @@ def data(url):
             response_company = requests.get(url_company)
             soup_company = BeautifulSoup(response_company.text, 'lxml')
             name = soup_company.find_all('div', class_='company-name')
-            named.append(name[0].text)
+            name1 = name[0].text
+            name1 = name1.replace('"',' ')
+            named.append(name1)
             ogrn = soup_company.find_all('span', class_='copy_target')[0]
             ogrnd.append(ogrn.text)
             okpo = soup_company.find(id='clip_okpo')
@@ -44,7 +47,40 @@ def data(url):
     dictionary = {'name': named, 'ogrn': ogrnd, 'okpo': okpod, 'status': statusd, 'date': dated, 'capital': capitald}
     return dictionary
 if __name__ == "__main__":
+    
     url_first = 'https://www.rusprofile.ru/codes/89220'
     url_second = 'https://www.rusprofile.ru/codes/429110'
     first_dict = data(url_first)
     second_dict = data(url_second)
+    connection = pymysql.connect(host='127.0.0.1',
+                                 user='root',
+                                 password='root',
+                                 db='sample',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            # Sql
+            for i in first_dict:
+                for j in range(len(first_dict[i])):
+                    name = first_dict[i][j]
+                    ogrn = first_dict[i][j]
+                    okpo = first_dict[i][j]
+                    status = first_dict[i][j]
+                    date = first_dict[i][j]
+                    capital = first_dict[i][j] 
+                    cursor.execute(f"INSERT INTO sample.data (name, ogrn, okpo, status, date, capital) VALUES (`{name}`,{ogrn},{okpo},{status},{date},{capital})")
+                break
+            for i in second_dict:
+                for j in range(len(second_dict[i])):
+                    name = second_dict[i][j]
+                    ogrn = second_dict[i][j]
+                    okpo = second_dict[i][j]
+                    status = second_dict[i][j]
+                    date = second_dict[i][j]
+                    capital = second_dict[i][j] 
+                    cursor.execute(f"INSERT INTO sample.data (name, ogrn, okpo, status, date, capital) VALUES (`{name}`,{ogrn},{okpo},{status},{date},{capital})")
+                break
+    finally:
+        # Закрыть соединение (Close connection).
+        connection.close()
